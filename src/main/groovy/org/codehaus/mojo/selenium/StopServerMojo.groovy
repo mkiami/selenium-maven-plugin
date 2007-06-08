c8ab4cdd3ea44644e9f19351f7ab26ea47cfd88a
user57 <user57@52ab4f32-60fc-0310-b215-8acea882cd1b>
2007-06-08 05:11:10 +0000

    (MSELENIUM-10) Add support to stop the server gracefully explicitly
    git-svn-id: https://svn.codehaus.org/mojo/trunk/mojo/selenium-maven-plugin@4350 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/groovy/org/codehaus/mojo/selenium/StopServerMojo.groovy
@@ -0,0 +1,52 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License") you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.codehaus.mojo.selenium
+
+import org.codehaus.mojo.groovy.GroovyMojoSupport
+
+/**
+ * Stop the Selenium server.
+ *
+ * @goal stop-server
+ *
+ * @version $Id$
+ */
+class StopServerMojo
+    extends GroovyMojoSupport
+{
+    /**
+     * The port number of the server to connect to.
+     *
+     * @parameter expression="${port}" default-value="4444"
+     */
+    int port
+
+    //
+    // Mojo
+    //
+
+    void execute() {
+        log.info('Stopping Selenium server...')
+        
+        def url = new URL("http://localhost:$port/selenium-server/driver/?cmd=shutDown")
+        
+        url.openConnection().content
+    }
+}
